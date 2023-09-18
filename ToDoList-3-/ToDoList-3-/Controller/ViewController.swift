8ddf1b15d3da447b96abf8848d3afdb0df6b4cb4
KangJiHun1028 <minsakr10@naver.com>
2023-09-18 16:08:08 +0900

    Profile 컨트롤러 연결

--- ToDoList-3-/ToDoList-3-/Controller/ViewController.swift
+++ ToDoList-3-/ToDoList-3-/Controller/ViewController.swift
@@ -33,6 +33,7 @@ class ViewController: UIViewController {
         return button
     }()
 
+    // 완료한 일 보기 버튼
     private var completeButton: UIButton = {
         let button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
@@ -44,6 +45,19 @@ class ViewController: UIViewController {
         return button
     }()
 
+    // 프로필 페이지 버튼
+    private var profileButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+
+        button.setTitle("프로필 페이지 가기", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Helvetica", size: 17)
+        button.addTarget(self, action: #selector(profileButtonTapped), for: .touchUpInside)
+
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -51,6 +65,7 @@ class ViewController: UIViewController {
         view.addSubview(mainLabel)
         view.addSubview(todoButton)
         view.addSubview(completeButton)
+        view.addSubview(profileButton)
         // 레이아웃 설정
         NSLayoutConstraint.activate([
             mainLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
@@ -58,12 +73,19 @@ class ViewController: UIViewController {
             todoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             todoButton.topAnchor.constraint(equalTo: mainLabel.bottomAnchor, constant: 200),
             completeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            completeButton.topAnchor.constraint(equalTo: todoButton.bottomAnchor, constant: 50)
+            completeButton.topAnchor.constraint(equalTo: todoButton.bottomAnchor, constant: 50),
+            profileButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            profileButton.topAnchor.constraint(equalTo: completeButton.bottomAnchor, constant: 50)
         ])
     }
 
     @objc private func todoGoButtonTapped() {
         let todoViewController = TodoViewController()
         navigationController?.pushViewController(todoViewController, animated: true)
+    }
+
+    @objc private func profileButtonTapped() {
+        let profileDesignViewController = ProfileDesignViewController()
+        navigationController?.pushViewController(profileDesignViewController, animated: true)
     }
 }
